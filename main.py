98f7a0346116ffdbfb261d4012e75c72b632e56c
vinayrvr987 <vinayrvr987@gmail.com>
2024-01-08 17:03:34 +0530

    Updated code to tell weather from API

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import subprocess
+import requests
+import json
 # import os
 
 def text_to_speech(text):
@@ -10,6 +12,17 @@ def text_to_speech(text):
 
     subprocess.run(["powershell", "-Command", command])
 
+def weather_api_process(city):
+    url = f'http://api.weatherapi.com/v1/current.json?key=d6c0d2026c90422e93e110353240801&q={city}&aqi=no'
+    
+    r = requests.get(url)
+    weather_dict_response = json.loads(r.text)
+    temperature = weather_dict_response["current"]["temp_c"]
+
+    text_to_speech(f"The current weather in {city} is {temperature} degrees. ")
+
+    return temperature
+
 if __name__ == '__main__':
     welcomeMsg = '''Welcome to RoboSpeaker 1.1 by Vinay
     To quit using robot type 'q'.
@@ -18,7 +31,8 @@ if __name__ == '__main__':
     print(welcomeMsg)
     text_to_speech(welcomeMsg)
     while(True):
-        user_input = input("Enter the text. So that I can read it aloud!: ")
-        if user_input == 'q':
+        city = input("Enter the City Name. So that I can give its temperature aloud!: ")
+        if city == 'q':
             break
-        text_to_speech(user_input)
+        text_to_speech(city)
+        print(weather_api_process(city))
